c6f5de62878f85eff7a017206509959623ae6baf
santo1989 <65486217+santo1989@users.noreply.github.com>
2022-01-17 22:25:26 +0600

    Initial commit

---
+++ app/src/main/java/com/santosystemsandsolutions/attendance/DbHelper.java
@@ -0,0 +1,109 @@
+package com.santosystemsandsolutions.attendance;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+
+import androidx.annotation.Nullable;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+
+    private static final int VERSION = 1;
+
+    //Class information Table
+    private static final String CLASS_TABLE_NAME = "CLASS_TABLE";
+    public static final String C_ID = "_CID";
+    public static final String CLASS_NAME_KEY = "CLASS_NAME";
+    public static final String SUBJECT_NAME_KEY = "SUBJECT_NAME";
+    private static final String CREATE_CLASS_TABLE =
+            " CREATE TABLE " + CLASS_TABLE_NAME + "(" +
+                    C_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    CLASS_NAME_KEY + " TEXT NOT NULL, " +
+                    SUBJECT_NAME_KEY + " TEXT NOT NULL, " +
+                    " UNIQUE (" + CLASS_NAME_KEY + "," + SUBJECT_NAME_KEY + ")" + ");";
+
+    private static final String DROP_CLASS_TABLE = " DROP TABLE IF EXISTS " + CLASS_TABLE_NAME;
+    private static final String SELECT_CLASS_TABLE = " SELECT * FROM " + CLASS_TABLE_NAME;
+
+//Student information Table
+
+    private static final String STUDENT_TABLE_NAME = "STUDENT_TABLE";
+    private static final String S_ID = "_SID";
+    private static final String STUDENT_NAME_KEY = "STUDENT_NAME";
+    private static final String STUDENT_ROLL_KEY = "ROLL";
+    private static final String CREATE_STUDENT_TABLE =
+            " CREATE TABLE " + STUDENT_TABLE_NAME + "(" +
+                    S_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    C_ID + " INTEGER NOT NULL, " +
+                    STUDENT_NAME_KEY + " TEXT NOT NULL," +
+                    STUDENT_ROLL_KEY + " INTEGER," +
+                    " FOREIGN KEY ( " + C_ID + ") REFERENCES " + CLASS_TABLE_NAME + "(" + C_ID + ")" +
+                    ");";
+
+    private static final String DROP_STUDENT_TABLE = " DROP TABLE IF EXISTS " + STUDENT_TABLE_NAME;
+    private static final String SELECT_STUDENT_TABLE = "SELECT * FROM " + STUDENT_TABLE_NAME;
+
+
+    //Status information Table
+
+    private static final String STATUS_TABLE_NAME = "STATUS_TABLE";
+    private static final String STATUS_ID = "_STATUS_ID";
+    private static final String DATE_KEY = "STATUS_DATE";
+    private static final String STATUS_KEY = "STATUS";
+    private static final String CREATE_STATUS_TABLE =
+            " CREATE TABLE " + STATUS_TABLE_NAME +
+                    "(" +
+                    STATUS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    S_ID + " INTEGER NOT NULL," +
+                    DATE_KEY + " DATE NOT NULL," +
+                    STATUS_KEY + " TEXT NOT NULL," +
+                    " UNIQUE (" + S_ID + "," + DATE_KEY + ")," +
+                    " FOREIGN KEY ( " + S_ID + ") REFERENCES " + STUDENT_TABLE_NAME + "(" + S_ID + ")" +
+                    ");";
+
+    private static final String DROP_STATUS_TABLE = " DROP TABLE IF EXISTS " + STATUS_TABLE_NAME;
+    private static final String SELECT_STATUS_TABLE = "SELECT * FROM " + STATUS_TABLE_NAME;
+
+
+    public DbHelper(@Nullable Context context) {
+        super(context, "Attendance.db", null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_CLASS_TABLE);
+        db.execSQL(CREATE_STUDENT_TABLE);
+        db.execSQL(CREATE_STATUS_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        try {
+            db.execSQL(DROP_CLASS_TABLE);
+            db.execSQL(DROP_STUDENT_TABLE);
+            db.execSQL(DROP_STATUS_TABLE);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    long addClass(String className,String subjectName){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(CLASS_NAME_KEY,className);
+        values.put(SUBJECT_NAME_KEY,subjectName);
+        return database.insert(CLASS_TABLE_NAME,null,values);
+
+    }
+
+    Cursor getClassTable(){
+        SQLiteDatabase database = this.getReadableDatabase();
+        return database.rawQuery(SELECT_CLASS_TABLE,null);
+
+    }
+}
---
+++ app/src/main/java/com/santosystemsandsolutions/attendance/MainActivity.java
@@ -0,0 +1,117 @@
+package com.santosystemsandsolutions.attendance;
+
+//import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+//import android.view.LayoutInflater;
+import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
+    RecyclerView recyclerView;
+    ClassAdapter classAdapter;
+    RecyclerView.LayoutManager LayoutManager;
+
+    ArrayList<ClassItem> classitems = new ArrayList<>();
+
+    Toolbar toolbar;
+
+    DbHelper dbHelper;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dbHelper = new DbHelper(this);
+
+        fab = findViewById(R.id.fab_main);
+        fab.setOnClickListener(v -> showDialog());
+        loadData();
+
+
+        recyclerView = findViewById(R.id.recycleview);
+        recyclerView.setHasFixedSize(true);
+        LayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(LayoutManager);
+        classAdapter = new ClassAdapter(this, classitems);
+        recyclerView.setAdapter(classAdapter);
+
+        classAdapter.setOnItemClickListener(position -> gotoIteamActivity(position));
+
+        SetToolbar();
+
+    }
+
+    private void loadData() {
+        Cursor cursor = dbHelper.getClassTable();
+
+        classitems.clear();
+        while (cursor.moveToNext()) {
+            int id = cursor.getInt(cursor.getColumnIndex(DbHelper.C_ID));
+            String className = cursor.getString(cursor.getColumnIndex(DbHelper.CLASS_NAME_KEY));
+            String subjectName = cursor.getString(cursor.getColumnIndex(DbHelper.SUBJECT_NAME_KEY));
+            classitems.add(new ClassItem(id, className, subjectName));
+        }
+
+    }
+
+    private void SetToolbar() {
+
+        toolbar = findViewById(R.id.toolbar);
+        TextView title = toolbar.findViewById(R.id.title_toolbar);
+        TextView subtitle = toolbar.findViewById(R.id.subtitle_toolbar);
+        ImageButton back = toolbar.findViewById(R.id.back);
+        ImageButton save = toolbar.findViewById(R.id.save);
+
+        title.setText("Student Attendance App");
+        subtitle.setText("Developer:\n Md. Hasibul Islam Santo\n B.Sc in CSE, 4th year 2nd Semester\n ID:13-0-52-020-003 ");
+        //subtitle.setVisibility(View.INVISIBLE);
+        back.setVisibility(View.INVISIBLE);
+        save.setVisibility(View.INVISIBLE);
+
+
+    }
+
+    private void gotoIteamActivity(int position) {
+        Intent intent = new Intent(this, StudentActivity.class);
+        intent.putExtra("className", classitems.get(position).getClassName());
+        intent.putExtra("subjectName", classitems.get(position).getSubjectName());
+        intent.putExtra("position", position);
+        startActivity(intent);
+
+    }
+
+    private void showDialog() {
+        MyDialog dialog = new MyDialog();
+        dialog.show(getSupportFragmentManager(), MyDialog.Class_ADD_DIALOG);
+        dialog.setListener((className, subjectName) -> addClass(className, subjectName));
+
+
+    }
+
+    private void addClass(String className, String subjectName) {
+
+        long cid = dbHelper.addClass(className, subjectName);
+        ClassItem classItem = new ClassItem(cid, className, subjectName);
+        classitems.add(classItem);
+        classAdapter.notifyDataSetChanged();
+
+    }
+}
